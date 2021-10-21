92b60214a6f232b6c87c1425982aa27c0105bfdc
olga27zl <olga27zl@gmail.com>
2021-10-21 17:16:28 +0300

    fix ami value

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "linux-instance" {
-  ami           = " ami-06ec8443c2a35b0ba"
+  ami           = "ami-06ec8443c2a35b0ba"
   instance_type = "t2.micro"
 
   subnet_id = aws_subnet.lesson12-subnet-public-3.id
